6d5c24e6790800d97584f61904a260efd605ff9f
EduardoMVAz <emendesvaz@gmail.com>
2024-02-23 08:32:44 -0300

    ADDITIONS: LoginIn

--- src/main/java/max/account/AccountController.java
+++ src/main/java/max/account/AccountController.java
@@ -16,6 +16,9 @@ public interface AccountController {
     @PostMapping("/accounts")
     public ResponseEntity<AccountOut> create(@RequestBody(required = true) AccountIn accountIn);
 
+    @PostMapping("/accounts/login")
+    public ResponseEntity<AccountOut> login(@RequestBody(required = true) LoginIn in);
+
     @PutMapping("/accounts/{id}")
     public ResponseEntity<AccountOut> update(@PathVariable(required = true) String id, @RequestBody(required = true) AccountIn accountIn);
 
